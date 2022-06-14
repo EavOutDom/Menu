c6a2785580ba1f966ee36546a7cdbc9b80a4fb7e
EavOutDom <eavoutdom400@gmail.com>
2022-06-14 20:09:07 +0700

    commit 3

--- src/categories.jsx
+++ src/categories.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Categories = ({ selectCategory, categoriesList }) => {
     // console.log(categoriesList.length);
+    const [value, setValue] = useState(0);
     return (
         <div className="my-4">
             {categoriesList.map((data, index) => {
                 return (
-                    <button key={index} onClick={() => selectCategory(data)}>
+                    <button key={index} onClick={() => {selectCategory(data); setValue(index)}}
+                    className={`${index === value && 'underline bg-white ring-2 ring-white'}`}
+                    >
                         {data}
                     </button>
                 );
